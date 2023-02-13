54a551740d75cccb44742c7f2c5c310d2a8fde1e
woerwuoeriuwer <125259030+woerwuoeriuwer@users.noreply.github.com>
2023-02-13 11:23:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Dies, Das, verschiedene Dinge')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
+
