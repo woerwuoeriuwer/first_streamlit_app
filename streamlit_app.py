30b020f3b8081d89e32afa135f80065a4ea0bbf6
woerwuoeriuwer <125259030+woerwuoeriuwer@users.noreply.github.com>
2023-02-13 11:26:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,12 @@ streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Dies, Das, verschiedene Dinge')
 
+streamlit.header('Customize Your Own Smoothie')
+
+
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.multiselect('Pick Fruits: ', list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
 
