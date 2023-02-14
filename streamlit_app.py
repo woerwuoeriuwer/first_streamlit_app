e79992f84b9ed7cbd24e3bab6a1dde57c21877b6
woerwuoeriuwer <125259030+woerwuoeriuwer@users.noreply.github.com>
2023-02-14 14:20:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list;")
-my_data = my_cur.fetchall()
-streamlit.dataframe(my_data)
+//my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+//my_cur = my_cnx.cursor()
+//my_cur.execute("select * from fruit_load_list;")
+//my_data = my_cur.fetchall()
+//streamlit.dataframe(my_data)
 
 
 
@@ -34,4 +34,6 @@ streamlit.text(f'The user entered {fruit_choice}')
 fruityvice_response = pd.json_normalize(requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}").json())
 
 streamlit.dataframe(fruityvice_response)
+
+add_fruit = streamlit.text_input("Which fruit would you like to add?", 'jackfruit')
 
